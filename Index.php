4a5f9353f62aaea1bb3084c25738001c2e41ea98
locky17 <69635199+locky17@users.noreply.github.com>
2021-03-12 11:06:36 +0200

    new site

---
+++ Index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="Assets/css/Style.css">
+    <link rel="stylesheet" href="Assets/css/normalize.css">
+    <link rel="stylesheet" href="Assets/css/slider.css">
+    <link rel="stylesheet" href="Assets/css/header.css">
+    <link rel="stylesheet" href="Assets/css/player.css">
+    <link rel="stylesheet" href="Assets/css/works.css">
+    <link rel="stylesheet" href="Assets/css/main.css">
+    <link rel="stylesheet" href="Assets/css/praim.css">
+    <link rel="stylesheet" href="Assets/css/soc.css">
+    <link rel="stylesheet" href="Assets/css/snake.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <header>
+
+        <div class="headerr_intro">
+            <h1 class="intro_text">
+                Rozhko
+            </h1>
+            <div id="slider">
+                <ul>
+                    <li style=" color: #000; background: transparent; ">html</li>
+                    <li style=" color: #000;background: transparent; ">css</li>
+                    <li style=" color: #000;background: transparent;">js</li>
+                    <li style=" color: #000;background: transparent; ">bootstrap</li>
+                    <li style=" color: #000;background: transparent;" font-size: 25px>php</li>
+                    <li style=" color: #000;background: transparent;" font-size: 25px>wordpress</li>
+                </ul>
+            </div>
+            <img class="img_header_logo" src="Assets/img/Twyn.gif" alt="">
+        </div>
+        <div class="bnt-header">
+            <a href="#" class="animated-button thar-one">Обратная связь</a>
+        </div>
+
+        <button style="
+    color: #000;
+    position: absolute;
+    right: 55px;
+    margin-top: 55px;
+  
+" class=" dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Ru
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">En</a></li>
+        </ul>
+
+
+
+
+
+
+    </header>
+    <div class="backmain">
+        <main>
+            <div class="soc_items">
+
+                <a href="https://t.me/Taitakeee"> <img style="height: 25px;
+                             width: 25px;" src="Assets/img/svg/telegram%20(3).svg" alt=""></a>
+
+                <a href="https://steamcommunity.com/id/1010010101101001/"> <img style="height: 25px;
+                             width: 25px;" src="Assets/img/svg/steam%20(3).svg" alt=""></a>
+            </div>
+            <div class="works">
+            
+                    <div class="works_content">
+                        <div class="works_title">Работы</div>
+                        <div class="works_images">
+
+                            <div class="works_item">
+
+                                <a href="./pojects/1.html">
+                                    <div class="works_img">
+
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="works_item">
+
+                                <a href="./pojects/2.html">
+                                    <div class="works_img1">
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+               
+                <div style="margin: 179px 0 0 0;" class="praim">
+
+                    <h1>преимущества</h1>
+                    <div class="prime_content">
+                        <div style=" " class="prime_img">
+                            <img style="" class="prime_imgj1" src="Assets/img/fast.gif" alt="">
+
+                            <span class="prime_text">Быстрое выполнение заданий</span>
+                        </div>
+                        <div style=" " class="prime_img">
+                            <img style="" class="prime_imgj1" src="Assets/img/brain.gif" alt="">
+
+                            <span class="prime_text">
+                                Комуникабельность
+                            </span>
+                        </div>
+                        <div style=" " class="prime_img">
+                            <img style="" class="prime_imgj1" src="Assets/img/tenor.gif" alt="">
+
+                            <span style="   " class="prime_text">Чёткое выполнение вех заданий</span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+               
+
+
+
+
+
+            </div>
+        </main>
+    </div>
+    <script src="Assets/js/App.js"></script>
+    <script src="Assets/js/slider.js"></script>
+    <script src="Assets/js/player.js"></script>
+    <script src="Assets/js/snake.js"></script>
+</body>
+
+</html>
